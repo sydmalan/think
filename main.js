607849e30e9ebf261165148776511e3112873f24
sydmalan <mal19002@byui.edu>
2022-12-07 11:17:38 -0700

    changes

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
   
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
     if (!e.target.matches('.dropbtn')) {
     var myDropdown = document.getElementById("myDropdown");
       if (myDropdown.classList.contains('show')) {
@@ -14,8 +14,26 @@ function myFunction() {
     }
 }
 
-//shows the year for the copyright 
+// Shows the year for the copyright 
 let date = new Date();
 let year = date.getFullYear();
 
 document.querySelector('.year').textContent = year;
+
+
+
+// Accordion for the FAQ
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
