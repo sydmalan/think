7680e5a43266748e314c234a3760007b8bb90bbc
sydmalan <mal19002@byui.edu>
2022-12-14 00:26:45 -0700

    fixing json and adding it to the pricing page

--- unisex.js
+++ unisex.js
@@ -1,42 +1,84 @@
 // ASYNC AWAIT Fetch 
 
-const requestURL = ''
+const requestURL = 'https://sydmalan.github.io/think/unisex.json'
 
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json'
-
-async function getProphets(requestURL){
+async function get_unisex(requestURL){
     const response = await fetch(requestURL);
     console.log(response);
     if (response.ok){
         const jsObject = await response.json();
         console.log(jsObject);
 
-        const prophets = jsObject['prophets'];
-        // console.log(prophets[0].lastname);
+        const unisex = jsObject['unisex_shirts'];
 
-        prophets.forEach(displayProphets);
+        unisex.forEach(display_unisex);
     }
 };
 
-getProphets(requestURL);
+get_unisex(requestURL);
 
-function displayProphets(item){
+function display_unisex(item){
     let card = document.createElement('section');
-    let name = document.createElement('h2');
-    let birthdate = document.createElement('p');
-    let birthplace = document.createElement('p');
     let img = document.createElement('img');
+    let brand = document.createElement('p');
+    let desc = document.createElement('p');
+    let fabric = document.createElement('p');
+    let code = document.createElement('p');
 
-    name.textContent = item.name + ' ' + item.lastname;
-    birthdate.textContent = item.birthdate;
-    birthplace.textContent = item.birthplace;
-    img.setAttribute('src', item.imageurl);
-    img.setAttribute('alt', item.name + ' ' + item.lastname);
+    img.setAttribute('src', item.image);
+    img.setAttribute('alt', item.brand + ' ' + item.desc);
+    brand.textContent = item.brand;
+    desc.textContent = item.description;
+    fabric.textContent = item.fabric;
+    code.textContent = item.code;
 
-    card.appendChild(name);
-    card.appendChild(birthdate);
-    card.appendChild(birthplace);
     card.appendChild(img);
+    card.appendChild(brand);
+    card.appendChild(desc);
+    card.appendChild(fabric);
+    card.appendChild(code);
 
-    document.querySelector('.cards').appendChild(card);    
+    document.querySelector('.cards').appendChild(card); 
+
+
 }
+
+
+// const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json'
+
+// async function getProphets(requestURL){
+//     const response = await fetch(requestURL);
+//     console.log(response);
+//     if (response.ok){
+//         const jsObject = await response.json();
+//         console.log(jsObject);
+
+//         const prophets = jsObject['prophets'];
+//         // console.log(prophets[0].lastname);
+
+//         prophets.forEach(displayProphets);
+//     }
+// };
+
+// getProphets(requestURL);
+
+// function displayProphets(item){
+//     let card = document.createElement('section');
+//     let name = document.createElement('h2');
+//     let birthdate = document.createElement('p');
+//     let birthplace = document.createElement('p');
+//     let img = document.createElement('img');
+
+//     name.textContent = item.name + ' ' + item.lastname;
+//     birthdate.textContent = item.birthdate;
+//     birthplace.textContent = item.birthplace;
+//     img.setAttribute('src', item.imageurl);
+//     img.setAttribute('alt', item.name + ' ' + item.lastname);
+
+//     card.appendChild(name);
+//     card.appendChild(birthdate);
+//     card.appendChild(birthplace);
+//     card.appendChild(img);
+
+//     document.querySelector('.cards').appendChild(card);    
+// } 
